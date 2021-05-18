d8fd2fcdfb9beef4a2689f589b556dcb80f6222d
Kaveen-Madhusanka <56001178+Kaveen-Madhusanka@users.noreply.github.com>
2021-05-18 20:03:04 +0530

    Add project files.

---
+++ SeriLogExample/Form1.cs
@@ -0,0 +1,33 @@
+﻿using Log;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeriLogExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                Logger.Write(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.File(@"D:\games\logFile.log").CreateLogger();
+            Log.Information("Testing header");
+            try
+            {
+                Log.Debug("something");
+                throw new NotImplementedException();
+                
+               
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex,"Error kaveen");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+                Console.WriteLine("Completed");
+                Console.ReadKey();
+            }
+        }
+    }
+}
