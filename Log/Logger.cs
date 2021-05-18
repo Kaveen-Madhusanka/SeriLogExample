edb7602e51a1e7fbcdadc1af096d3e726b9d5dc6
Kaveen-Madhusanka <56001178+Kaveen-Madhusanka@users.noreply.github.com>
2021-05-18 20:13:57 +0530

    Add gitv ignore

---
+++ Log/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+using Serilog.Core;
+namespace Log
+{
+    public static class Logger
+    {
+        public static void Write(string text)
+        {
+            var logger = new LoggerConfiguration().WriteTo.File(@"D:\Songs\Vedio\english\logFile.log").CreateLogger();
+            logger.Information("Testing header");
+            logger.Error(text);
+        }
+    }
+}
